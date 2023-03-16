590a9c406049626b81b37f94ae759446814151e0
Cristiano <kristian0tj2019@gmail.com>
2023-03-16 13:40:04 -0300

    persistência de dados com postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [
-    UsersModule,
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db',
-      entities: [__dirname + '/**/*.entity.js'],
-      synchronize: true,
-    }),
-  ],
+  imports: [UsersModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
